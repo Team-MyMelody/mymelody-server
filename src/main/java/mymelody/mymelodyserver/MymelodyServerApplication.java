78beaffeba26403c3b4dca3e10ab57ce3596b58a
이윤서 <yoonsseo@192.168.nate.com>
2024-04-16 16:49:30 +0900

    initial commit

---
+++ src/main/java/mymelody/mymelodyserver/MymelodyServerApplication.java
@@ -0,0 +1,13 @@
+package mymelody.mymelodyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymelodyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymelodyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mymelody/mymelodyserver/MymelodyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mymelody.mymelodyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymelodyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
