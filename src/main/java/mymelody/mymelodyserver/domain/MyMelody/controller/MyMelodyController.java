9b188d32264214c85c2c8dd291e2e0b5bc1867a9
letskuku <yujung9288@naver.com>
2024-06-01 04:50:41 +0900

    chore: 필요없는 코드 삭제 #3

--- src/main/java/mymelody/mymelodyserver/domain/MyMelody/controller/MyMelodyController.java
+++ src/main/java/mymelody/mymelodyserver/domain/MyMelody/controller/MyMelodyController.java
@@ -5,11 +5,10 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mymelody.mymelodyserver.domain.MyMelody.dto.response.GetMyMelodiesByLocation;
-import mymelody.mymelodyserver.domain.MyMelody.dto.response.MyMelodyInfo;
 import mymelody.mymelodyserver.domain.MyMelody.service.MyMelodyService;
+import mymelody.mymelodyserver.global.common.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +22,7 @@ public class MyMelodyController {
 
     private final MyMelodyService myMelodyService;
 
-    @Operation(summary = "반경 1km 내의 마이멜로디 목록 조회(페이지네이션)")
+    @Operation(summary = "반경 1km 내의 마이멜로디 목록 조회")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "조회 성공")
     })
@@ -33,25 +32,10 @@ public class MyMelodyController {
             @Parameter(name = "page", description = "조회할 페이지 위치, default는 1"),
             @Parameter(name = "size", description = "조회할 목록 사이즈, default는 10")
     })
-    @GetMapping("/location/page")
-    public ResponseEntity<GetMyMelodiesByLocation> getMyMelodiesByLocation(@RequestParam double latitude,
-            @RequestParam double longitude, @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size) {
-        return ResponseEntity.ok(myMelodyService.getMyMelodiesByLocationWithPagination(latitude,
-                longitude, page, size));
-    }
-
-    @Operation(summary = "반경 1km 내의 마이멜로디 목록 조회")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "조회 성공")
-    })
-    @Parameters({
-            @Parameter(name = "latitude", description = "사용자의 현재 위치 위도"),
-            @Parameter(name = "longitude", description = "사용자의 현재 위치 경도")
-    })
     @GetMapping("/location")
-    public ResponseEntity<List<MyMelodyInfo>> getMyMelodiesByLocation(@RequestParam double latitude,
-            @RequestParam double longitude) {
-        return ResponseEntity.ok(myMelodyService.getMyMelodiesByLocation(latitude, longitude));
+    public ResponseEntity<GetMyMelodiesByLocation> getMyMelodiesByLocation(@RequestParam double latitude,
+            @RequestParam double longitude, PageRequest pageRequest) {
+        return ResponseEntity.ok(myMelodyService.getMyMelodiesByLocationWithPagination(latitude,
+                longitude, pageRequest));
     }
 }
--- src/main/java/mymelody/mymelodyserver/domain/MyMelody/service/MyMelodyService.java
+++ src/main/java/mymelody/mymelodyserver/domain/MyMelody/service/MyMelodyService.java
@@ -1,13 +1,11 @@
 package mymelody.mymelodyserver.domain.MyMelody.service;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mymelody.mymelodyserver.domain.MyMelody.dto.response.GetMyMelodiesByLocation;
-import mymelody.mymelodyserver.domain.MyMelody.dto.response.MyMelodyInfo;
 import mymelody.mymelodyserver.domain.MyMelody.entity.MyMelody;
 import mymelody.mymelodyserver.domain.MyMelody.repository.MyMelodyRepository;
+import mymelody.mymelodyserver.global.common.PageRequest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,16 +18,10 @@ public class MyMelodyService {
     private final MyMelodyRepository myMelodyRepository;
 
     public GetMyMelodiesByLocation getMyMelodiesByLocationWithPagination(double latitude, double longitude,
-            int page, int size) {
-        Pageable pageable = PageRequest.of(page, size);
+            PageRequest pageRequest) {
+        Pageable pageable = pageRequest.of();
         Page<MyMelody> myMelodies = myMelodyRepository.findAllByRange1km(latitude, longitude, pageable);
 
         return GetMyMelodiesByLocation.of(myMelodies);
-    }
-
-    public List<MyMelodyInfo> getMyMelodiesByLocation(double latitude, double longitude) {
-        List<MyMelody> myMelodies = myMelodyRepository.findAllByRange1km(latitude, longitude);
-
-        return MyMelodyInfo.of(myMelodies);
     }
 }
