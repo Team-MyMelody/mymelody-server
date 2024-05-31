ecca8ff202ec402f653b0563150d19c1b6aad5cb
letskuku <yujung9288@naver.com>
2024-06-01 03:49:00 +0900

    feat: 계층 간 반경 1km 내 마이멜로디 목록 이동 위한 dto 구현 #2

---
+++ src/main/java/mymelody/mymelodyserver/domain/MyMelody/dto/response/MyMelodyInfo.java
@@ -0,0 +1,30 @@
+package mymelody.mymelodyserver.domain.MyMelody.dto.response;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import mymelody.mymelodyserver.domain.MyMelody.entity.MyMelody;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class MyMelodyInfo {
+
+    private final Long myMelodyId;
+    private final double latitude;
+    private final double longitude;
+    private final String comment;
+
+    private final Long memberId;
+    private final String nickname;
+
+    private final String isrc;
+
+    public static List<MyMelodyInfo> of(List<MyMelody> myMelodies) {
+        return myMelodies.stream()
+                .map(myMelody -> new MyMelodyInfo(myMelody.getId(), myMelody.getLatitude(),
+                        myMelody.getLongitude(), myMelody.getComment(), myMelody.getMember().getId(),
+                        myMelody.getMember().getNickname(), myMelody.getMusic().getIsrc()))
+                .toList();
+    }
+}
