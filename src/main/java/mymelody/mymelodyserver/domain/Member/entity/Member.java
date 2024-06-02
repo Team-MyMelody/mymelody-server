0727f7fda73cae49758980a0e0226bac0b7d10b2
yoonsseo <yellisa2000@gmail.com>
2024-06-03 02:17:20 +0900

    feat: spotify 사용자 식별값 필드 추가 #7

--- src/main/java/mymelody/mymelodyserver/domain/Member/entity/Member.java
+++ src/main/java/mymelody/mymelodyserver/domain/Member/entity/Member.java
@@ -1,21 +1,28 @@
 package mymelody.mymelodyserver.domain.Member.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import mymelody.mymelodyserver.global.entity.BaseTimeEntity;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Member extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
 
+    private String spotifyId;
+//    private String spotifyAccessToken;
     private String name;
     private String nickname;
     private String email;
+
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("\"USER\"")
+    private Role role;
 }
