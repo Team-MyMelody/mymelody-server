9bb37f5f915af1631a5b852ed8d4ded6ee39c5fc
yoonsseo <yellisa2000@gmail.com>
2024-06-03 02:23:21 +0900

    feat: spotify 사용자 식별값으로 사용자 조회하는 메소드 추가 #7

--- src/main/java/mymelody/mymelodyserver/domain/Member/repository/MemberRepository.java
+++ src/main/java/mymelody/mymelodyserver/domain/Member/repository/MemberRepository.java
@@ -4,7 +4,10 @@ import mymelody.mymelodyserver.domain.Member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findBySpotifyId(String spotifyId);
 
 }
