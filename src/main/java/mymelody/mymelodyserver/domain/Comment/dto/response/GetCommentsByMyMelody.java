f8c8f957437e7d12053fe3db8c333e948d882b4e
letskuku <yujung9288@naver.com>
2024-06-02 11:15:20 +0900

    feat: 계층 간 comment 정보 이동 위한 dto 구현 #5

---
+++ src/main/java/mymelody/mymelodyserver/domain/Comment/dto/response/GetCommentsByMyMelody.java
@@ -0,0 +1,22 @@
+package mymelody.mymelodyserver.domain.Comment.dto.response;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import mymelody.mymelodyserver.domain.Comment.entity.Comment;
+import org.springframework.data.domain.Page;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class GetCommentsByMyMelody {
+
+    private final int totalPages;
+    private final long totalElements;
+    private final List<CommentInfo> commentInfos;
+
+    public static GetCommentsByMyMelody of(Page<Comment> comments) {
+        return new GetCommentsByMyMelody(comments.getTotalPages(), comments.getTotalElements(),
+                CommentInfo.of(comments.getContent()));
+    }
+}
