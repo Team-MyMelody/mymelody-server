6bb976c0541007e538ec81e912b6eea966f1d6dc
letskuku <yujung9288@naver.com>
2024-11-07 00:39:38 +0900

    feat: LikesRepository 구현 및 에러 코드 추가 #10

--- src/main/java/mymelody/mymelodyserver/global/entity/ErrorCode.java
+++ src/main/java/mymelody/mymelodyserver/global/entity/ErrorCode.java
@@ -16,7 +16,8 @@ public enum ErrorCode {
 
     // 404 NOT FOUND
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
-    MYMELODY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 마이멜로디를 찾을 수 없습니다.");
+    MYMELODY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 마이멜로디를 찾을 수 없습니다."),
+    LIKES_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 마이멜로디에 대한 좋아요를 찾을 수 없습니다.");
 
     // 409 CONFLICT
 
