133a11ae0b7fa86570a82b9e1a8dc1495eba2b6e
yoonsseo <yellisa2000@gmail.com>
2024-11-09 18:33:06 +0900

    feat: api 호출 로그 추가

--- src/main/java/mymelody/mymelodyserver/global/auth/controller/AuthController.java
+++ src/main/java/mymelody/mymelodyserver/global/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package mymelody.mymelodyserver.global.auth.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mymelody.mymelodyserver.global.auth.dto.response.TokenDto;
 import mymelody.mymelodyserver.global.auth.jwt.JwtTokenProvider;
 import mymelody.mymelodyserver.global.auth.security.CustomUserDetails;
@@ -15,17 +16,19 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 @Tag(name = "Auth", description = "로그인 API")
+@Slf4j
 public class AuthController {
     private final AuthService authService;
     private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/login")
-    public ResponseEntity<TokenDto> signIn(@RequestParam("code") String code) {
+    public ResponseEntity<TokenDto> login(@RequestParam("code") String code) {
         return authService.signIn(code);
     }
 
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(HttpServletRequest request) {
+        log.info("[AuthController] logout");
         String token = jwtTokenProvider.resolveToken(request);
         return authService.logout(token);
     }
